671ba51b46b32799febdb8c2ef22e7ce24b6692d
Polina <pelageya913@gmail.com>
2017-02-06 19:44:56 +0200

    glyphs

--- markup/js/main.js
+++ markup/js/main.js
@@ -1,34 +1,9 @@
-// $(document).ready(function() {
-//     $(function() {
-//
-//         console.log($('.jcarousel')[0]);
-//
-//         $('.jcarousel').jcarousel();
-//     });
-// });
-
-
-$(document).ready(function(){
-    $('.regular.slider').slick({
-
+$( document ).ready(function() {
+    $('.new-carousel').slick({
+           dots: true,
+           slidesToShow: 4,
+           slidesToScroll: 1,
+           autoplay: true,
+           autoplaySpeed: 2000,
     });
 });
-
-
-/*$(document).ready(function() {
-
-    $('.new-carousel').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-    });
-
-    // $('.new-carousel').slick({
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    //     autoplay: true,
-    //     autoplaySpeed: 2000,
-    // });
-});*/
-
