0c4395fefb77b39494cd74952521a1f0165a6375
vbalabanov <Viacheslav.Balabanov@logianalytics.com>
2017-02-08 20:59:20 +0200

    Templating and partially moving content

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $('.new-carousel').slick({
+           dots: true,
+           slidesToShow: 4,
+           slidesToScroll: 1,
+           autoplay: true,
+           autoplaySpeed: 2000,
+    });
+});
