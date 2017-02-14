537a3b4d9c5124867982e0d0ae68959c4294f0f1
DESKTOP-QS6BQVI\Daedalus <iv.y.magda@gmail.com>
2017-02-14 06:18:35 +0200

    Create new product functional

--- controllers/default.js
+++ controllers/default.js
@@ -4,7 +4,8 @@ exports.install = function () {
     F.route('/');
     F.route('/products', view_products_list);
     F.route('/products/{product_id}', view_product);
-    F.route('/products/create', view_product_add, ['post']);
+    F.route('/products/create', view_product_add);
+    F.route('/products/create', view_product_create, ['post']);
     F.route('/products/update/{product_id}', view_product_update, ['put']);
     F.route('/products/delete/{product_id}', view_product_delete, ['delete']);
 };
@@ -25,12 +26,17 @@ function view_product(product_id) {
 }
 
 function view_product_add() {
-    //var self = this;
-    //Product.create_new(this.body, function (result) {
-    //    self.json(SUCCESS(result));
-    //});
-    //console.log("from server product create");
-    //console.log(this.body);
+    var self = this;
+    self.view('/temp/product_add');
+}
+
+function view_product_create() {
+    var self = this;
+    Product.create_new(this.body, function (result) {
+        self.json(SUCCESS(result));
+    });
+    console.log("from server product");
+    console.log(this.body);
 }
 
 function view_product_update() {
