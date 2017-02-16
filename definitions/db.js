f2610f4938c50a7d26b1f0b4904874543c455317
DESKTOP-QS6BQVI\Daedalus <iv.y.magda@gmail.com>
2017-02-16 11:24:04 +0200

    Create table

--- definitions/db.js
+++ definitions/db.js
@@ -1,40 +1,31 @@
-/*var mysql = require('mysql');
-
-var pool = mysql.createPool({
-    host                : 'localhost',
-    user                : 'root',
-    password            : 'root',
-    database            : 'outlet',
-    multipleStatements  :  true
-});
-
-F.database = function (callback) {
-    return pool.getConnection(callback);
-};
-
-F.emit('initdb');*/
 var fs = require('fs');
 
-var config = {
+/*var config = {
     driver: 'msnodesqlv8',
     connectionString: F.config.DEVSQLSERVER
+
+};*/
+
+var config = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'outlet',
+    multipleStatements: true
 };
 
-var sqlAgent = require('sqlagent/sqlserver').init(config, true);
+var sqlAgent = require('sqlagent/mysql').init(config, true);
 
-fs.readFile('./definitions/db_script.sql', 'utf8', function (err, data) {
+fs.readFile('./definitions/mysql_sript.txt', 'utf8', function (err, data) {
     if (err) throw err;
-    //console.log(data);
 
     var sql_query = data;
+    var sql = DATABASE();
 
-    var sql = DATABASE(null);
-    sql.query(sql_query).make(function (builder) { console.log('query data'); });
+    sql.query(sql_query);
     sql.exec(function (err, response) {
         console.log('exec data');
         F.emit('initdb');
-        //console.log(response.allProducts);
         console.log('initdb!');
     });
 });
-
