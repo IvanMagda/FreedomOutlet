e26faa45d62a77733bdcfbf46345f11624f04cc9
vbalabanov <Viacheslav.Balabanov@logianalytics.com>
2017-02-16 17:48:13 +0200

    MSSQL db provider support !!!changes to sql agent npm package - sqlserver.js!!!

--- definitions/db.js
+++ definitions/db.js
@@ -1,28 +1,37 @@
 var fs = require('fs');
 
-/*var config = {
-    driver: 'msnodesqlv8',
-    connectionString: F.config.DEVSQLSERVER
+var config;
 
-};*/
+switch (F.config.DBPROVIDER) {
+    case "mysql":
+        config = {
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'outlet',
+            multipleStatements: true
+        };
+        break;
+    case "sqlserver":
+        config = {
+            driver: 'msnodesqlv8',
+            connectionString: F.config.DEVSQLSERVER
 
-var config = {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'outlet',
-    multipleStatements: true
-};
+        };
+        break;
+    default:
+        throw "Provider not supported";
+}
 
-var sqlAgent = require('sqlagent/mysql').init(config, true);
+var sqlAgent = require('sqlagent/' + F.config.DBPROVIDER).init(config, true);
 
-fs.readFile('./definitions/mysql_sript.txt', 'utf8', function (err, data) {
+fs.readFile('./definitions/dbinit_' + F.config.DBPROVIDER+'.txt', 'utf8', function (err, data) {
     if (err) throw err;
 
     var sql_query = data;
     var sql = DATABASE();
 
-    sql.query(sql_query);
+    sql.query("init", sql_query).make(function (builder) { });
     sql.exec(function (err, response) {
         console.log('exec data');
         F.emit('initdb');
