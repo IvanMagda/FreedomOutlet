c7c4141b69cdd46d826c0ba4b64c5abf3e85afd3
Polina <pelageya913@gmail.com>
2017-02-26 13:52:57 +0200

    -

--- markup/js/main.js
+++ markup/js/main.js
@@ -1,9 +1,18 @@
 $( document ).ready(function() {
-    $('.new-carousel').slick({
+    $('.main-page-carousel').slick({
            dots: true,
            slidesToShow: 4,
            slidesToScroll: 1,
            // autoplay: true,
            // autoplaySpeed: 2000,
+    });
+
+    $('.product-card-carousel').slick({
+        //dots: true,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
     });
 });
