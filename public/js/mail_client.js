f8d53465517fba6babf41fdd29aa832a4acabb0e
DESKTOP-QS6BQVI\Daedalus <iv.y.magda@gmail.com>
2017-03-02 02:24:48 +0200

    Mail validate

--- public/js/mail_client.js
+++ public/js/mail_client.js
@@ -10,3 +10,17 @@ $(document).ready(function () {
         });
     });
 });
+
+function validateForm() {
+    var name = document.forms["mailForm"]["name"].value;
+    var phone = document.forms["mailForm"]["phone"].value;
+    var message = document.forms["mailForm"]["message"].value;
+
+    if (name == "" || phone == "" || message == "") {
+        document.getElementById('validateGlobal').style.visibility = "visible";
+        document.getElementById('validatePhone').style.visibility = "visible";
+        document.getElementById('validateMessage').style.visibility = "visible";
+        document.getElementById('validateName').style.visibility = "visible";
+        return false;
+    }
+}
