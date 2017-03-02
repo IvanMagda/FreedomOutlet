e2926342d16df6c8b9552b64889ab3f0b581cff6
Polina <pelageya913@gmail.com>
2017-03-02 13:17:03 +0200

    product card

--- tmp/js-main.js
+++ tmp/js-main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){ $('.new-carousel').slick({dots:true,slidesToShow:4,slidesToScroll:1,autoplaySpeed:2000,})});
+$(document).ready(function(){ $('.main-page-carousel').slick({dots:true,slidesToShow:4,slidesToScroll:1,autoplaySpeed:2000,})});
