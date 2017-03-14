de69b205b4d9df1e42874d0ffc9971fd51916648
DESKTOP-QS6BQVI\Daedalus <iv.y.magda@gmail.com>
2017-03-14 02:21:15 +0200

    Modal validate

--- public/js/modal.js
+++ public/js/modal.js
@@ -26,15 +26,15 @@ $(document).ready(function () {
             );
     });
 
-    $('#register-form').find('button').click(function () {
-        $.post('/register', $('#reg-form').serialize(), function (answer) {
-            if (answer.success) {
-                window.location.href = '/';
-                return;
-            }
-            console.log(answer);
-        });
-    });
+    //$('#register-form').find('button').click(function () {
+    //    $.post('/register', $('#reg-form').serialize(), function (answer) {
+    //        if (answer.success) {
+    //            window.location.href = '/';
+    //            return;
+    //        }
+    //        console.log(answer);
+    //    });
+    //});
 
     $('#login-form-container').find('button').click(function () {
         $.post('/authorization', $('#login-form').serialize(), function (answer) {
@@ -63,6 +63,33 @@ function validate_modal_Form() {
         document.getElementById('validate_modal_Phone').style.visibility = "visible";
         document.getElementById('validate_modal_Message').style.visibility = "visible";
         document.getElementById('validate_modal_Name').style.visibility = "visible";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validate_register_Form() {
+    var login = document.forms["register_Form"].login;
+    var phone = document.forms["register_Form"].phone;
+    var mail = document.forms["register_Form"].mail;
+    var pass = document.forms["register_Form"].pass;
+
+    var inputs = { login, phone, mail, pass };
+    var validate_ids = ['validate_modal_Name', 'validate_modal_Phone', 'validate_modal_Mail', 'validate_modal_Pass', 'validate_modal_Global'];
+
+    //console.log(inputs);
+
+    if (login.value == "" || phone.value == "" || mail.value == "" || pass.value == "") {
+        for (var key in inputs) {
+            if (document.forms["register_Form"][key].value == "") {
+                document.forms["register_Form"][key].style.border = "1px solid red";
+            }
+        };
+        validate_ids.forEach(function (id) {
+            console.log(id);
+            document.getElementById(id).style.visibility = "visible";
+        });
         return false;
     } else {
         return true;
